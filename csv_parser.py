48581c29dc38452be1387b9dc981c0061e971632
AstralRomance <andrdoncwork@mail.ru>
2020-03-30 16:34:16 +0300

    Add files via upload

--- csv_parser.py
+++ csv_parser.py
@@ -1,14 +1,22 @@
 import sys
 
+from PyQt5.QtCore import QObject, pyqtSignal
 import pandas as pd
 
 
-class CsvReader:
-    def __init__(self, filepath='pred_intervals.csv'):
+class CsvReader(QObject):
+    dataframe_table = pyqtSignal(object)
+
+    def __init__(self, filepath=''):
+        super().__init__()
         self.filepath = filepath
 
-    def get_dataset(self):
-        return pd.read_csv(self.filepath)
+    def run(self):
+        try:
+            table_dataframe = pd.read_csv(self.filepath)
+            self.dataframe_table.emit(table_dataframe)
+        except FileNotFoundError:
+            return
 
     def change_file(self, filepath):
         self.filepath = filepath
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QMessageBox
+from PyQt5 import QtCore
 
 from MainWindow import *
 from csv_parser import CsvReader
@@ -10,33 +11,41 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
-        self.current_reader = CsvReader()
-
         self.ui.pushButton.clicked.connect(self.file_dialog)
 
-    def set_table_labels(self):
+        self.thread = QtCore.QThread()
+        self.current_reader = CsvReader()
+        self.current_reader.moveToThread(self.thread)
+        self.current_reader.dataframe_table.connect(self.set_table_labels)
+        self.thread.started.connect(self.current_reader.run)
+        self.thread.start()
+
+    @QtCore.pyqtSlot(object)
+    def set_table_labels(self, dataframe):
         try:
-            headers = self.current_reader.get_dataset().keys()
+            headers = dataframe.keys()
         except ValueError:
-            QMessageBox.about(self, 'Fatal Error', 'Value error raised while file reading')
+            QMessageBox.about(self, 'Exception', 'Value error raised while file reading')
             return
         self.ui.label.setText(self.current_reader.get_filepath())
         self.ui.tableWidget.setColumnCount(len(headers))
-        self.ui.tableWidget.setRowCount(len(self.current_reader.get_dataset()))
+        self.ui.tableWidget.setRowCount(len(dataframe))
         self.ui.tableWidget.setHorizontalHeaderLabels(headers)
-        for i in range(len(self.current_reader.get_dataset())):
-            for j in range(len(self.current_reader.get_dataset().columns)):
+        for i in range(len(dataframe)):
+            for j in range(len(dataframe.columns)):
                 self.ui.tableWidget.setItem(
                     i, j,
-                    QtWidgets.QTableWidgetItem(str(self.current_reader.get_dataset().iloc[i, j]))
+                    QtWidgets.QTableWidgetItem(str(dataframe.iloc[i, j]))
                                            )
         self.ui.tableWidget.resizeColumnsToContents()
 
     def file_dialog(self):
         fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', '')[0]
-        self.current_reader.change_file(fname)
-        self.set_table_labels()
+        try:
+            self.current_reader.change_file(fname)
+            self.current_reader.run()
+        except Exception as e:
+            print(f'{e} Some exception')
 
 
 if __name__ == "__main__":
